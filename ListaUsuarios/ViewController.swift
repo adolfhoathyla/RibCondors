c066bb39d20f260fbf33372d6e459070ffe73e53
Allef Sousa <allefsousa_1@hotmail.com>
2019-04-03 16:11:52 -0300

    Initial Commit

---
+++ ListaUsuarios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListaUsuarios
+//
+//  Created by Allef Sousa Santos on 03/04/19.
+//  Copyright © 2019 Allef Sousa Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
